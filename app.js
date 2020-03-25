46ea2660cd9c9173e9b6013ddf794e4f16c4914f
unknown <zckpp1992@gmail.com>
2020-03-25 16:11:05 -0400

    cookies

--- app.js
+++ app.js
@@ -28,7 +28,45 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('1234'));
+
+function auth(req, res, next) {
+  console.log(req.signedCookies);
+
+  if (!req.signedCookies.user) {
+    let authHeader = req.headers.authorization;
+    if (!authHeader) {
+      let err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+
+    let auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    let userName = auth[0];
+    let password = auth[1];
+    if (userName == 'admin' && password == 'password') {
+      res.cookie('user', 'admin', { signed: true });
+      next();
+    } else {
+      let err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+  } else {
+    if ('admin' == req.signedCookies.user) {
+      next();
+    } else {
+      let err = new Error('You are not the admin!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+  }
+}
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
