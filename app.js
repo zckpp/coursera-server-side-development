040899575acadb0694dfce834c160e1ca8ffc1bc
unknown <zckpp1992@gmail.com>
2020-05-30 16:10:39 -0400

    week 4

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
 var uploadRouter = require('./routes/uploadRouter');
+var favRouter = require('./routes/favRouter');
 
 // connect to MongoDB with mongoose
 const mongoose = require('mongoose');
@@ -57,6 +58,7 @@ app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites',favRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
